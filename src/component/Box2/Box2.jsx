f717f1966906fde71576c7c6f5e96bec17165cf6
APareja12 <alexis.pareja@hotmail.com>
2022-03-28 10:30:15 -0400

    adds drag and constraints

--- src/component/Box2/Box2.jsx
+++ src/component/Box2/Box2.jsx
@@ -6,6 +6,13 @@ export default function Box2() {
     <div className="box-container">
         <motion.div 
             className="box"
+            drag
+            dragConstraints={{
+                right: 20,
+                left: -20,
+                top: 5,
+                bottom: 5
+            }}
             whileHover={{
                 scale: 1.1
             }}
